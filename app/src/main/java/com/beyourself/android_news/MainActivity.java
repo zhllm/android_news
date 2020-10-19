f98ae624a9c7948ece2f7e8aa026dd533f3913d3
zhllm <beyourselfinsist@gmail.com>
2020-10-19 23:35:08 +0800

    stash

--- app/src/main/java/com/beyourself/android_news/MainActivity.java
+++ app/src/main/java/com/beyourself/android_news/MainActivity.java
@@ -2,53 +2,26 @@ package com.beyourself.android_news;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
+
+import com.beyourself.android_news.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewModelWithLiveData viewModelWithLiveData;
-    TextView textView;
-    ImageButton imageButtonLike, imageButtonDisLike;
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        imageButtonLike = findViewById(R.id.imageButton2);
-        imageButtonDisLike = findViewById(R.id.imageButton3);
-
-        viewModelWithLiveData =
-                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
-                        .get(ViewModelWithLiveData.class);
-
-        viewModelWithLiveData.getLikeNumber().observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(Integer integer) {
-                textView.setText(String.valueOf(integer));
-            }
-        });
-
-        imageButtonLike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModelWithLiveData.addLikeNumber(1);
-            }
-        });
-
-        imageButtonDisLike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModelWithLiveData.addLikeNumber(-1);
-            }
-        });
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        myViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
+                .get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
     }
 
     @Override
--- app/src/main/java/com/beyourself/android_news/MyViewModel.java
+++ app/src/main/java/com/beyourself/android_news/MyViewModel.java
@@ -1,7 +1,51 @@
 package com.beyourself.android_news;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
-    public int number = 0;
+    private MutableLiveData<Integer> aTeamScore;
+    private MutableLiveData<Integer> bTeamScore;
+    private int aBack, bBack;
+
+    public MutableLiveData<Integer> getATeamScore() {
+        if (aTeamScore == null) {
+            aTeamScore = new MutableLiveData<>();
+            aTeamScore.setValue(0);
+        }
+        return aTeamScore;
+    }
+
+    public MutableLiveData<Integer> getBTeamScore() {
+        if (bTeamScore == null) {
+            bTeamScore = new MutableLiveData<>();
+            bTeamScore.setValue(0);
+        }
+        return bTeamScore;
+    }
+
+    public void aTeamAdd(int n) {
+        aBack = aTeamScore.getValue();
+        bBack = bTeamScore.getValue();
+        aTeamScore.setValue(aTeamScore.getValue() + n);
+    }
+
+    public void bTeamAdd(int n) {
+        aBack = aTeamScore.getValue();
+        bBack = bTeamScore.getValue();
+        bTeamScore.setValue(bTeamScore.getValue() + n);
+    }
+
+    public void reset() {
+        aBack = aTeamScore.getValue();
+        bBack = bTeamScore.getValue();
+        aTeamScore.setValue(0);
+        bTeamScore.setValue(0);
+    }
+
+    public void undo() {
+        aTeamScore.setValue(aBack);
+        bTeamScore.setValue(bBack);
+    }
+
 }
