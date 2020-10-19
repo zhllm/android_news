9ff5bbaa68379188a51f2d8266a40a2f5aefce0e
zhllm <beyourselfinsist@gmail.com>
2020-10-19 21:50:14 +0800

    dataBinding

--- app/src/main/java/com/beyourself/android_news/MainActivity.java
+++ app/src/main/java/com/beyourself/android_news/MainActivity.java
@@ -2,53 +2,31 @@ package com.beyourself.android_news;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.beyourself.android_news.databinding.ActivityMainBinding;
 
-    ViewModelWithLiveData viewModelWithLiveData;
-    TextView textView;
-    ImageButton imageButtonLike, imageButtonDisLike;
+public class MainActivity extends AppCompatActivity {
+    MyViewModel myViewModel;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        imageButtonLike = findViewById(R.id.imageButton2);
-        imageButtonDisLike = findViewById(R.id.imageButton3);
 
-        viewModelWithLiveData =
-                new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
-                        .get(ViewModelWithLiveData.class);
-
-        viewModelWithLiveData.getLikeNumber().observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(Integer integer) {
-                textView.setText(String.valueOf(integer));
-            }
-        });
-
-        imageButtonLike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModelWithLiveData.addLikeNumber(1);
-            }
-        });
-
-        imageButtonDisLike.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewModelWithLiveData.addLikeNumber(-1);
-            }
-        });
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        myViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory())
+                .get(MyViewModel.class);
+        binding.setData(myViewModel);
+        binding.setLifecycleOwner(this);
     }
 
     @Override
--- app/src/main/java/com/beyourself/android_news/MyViewModel.java
+++ app/src/main/java/com/beyourself/android_news/MyViewModel.java
@@ -1,7 +1,20 @@
 package com.beyourself.android_news;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
-    public int number = 0;
+    private MutableLiveData<Integer> number;
+
+    public MutableLiveData<Integer> getNumber() {
+        if (number == null) {
+            number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+        return number;
+    }
+
+    public void addNumber(int n) {
+        number.setValue(number.getValue() + 1);
+    }
 }
